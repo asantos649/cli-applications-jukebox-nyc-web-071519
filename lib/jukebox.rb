df4707ded2c1a446ae0e28bd9e6066da48ef8121
Andrew Santos <asantos@alumni.nd.edu>
2019-07-12 16:53:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,7 +55,7 @@ def run(array)
     elsif reply == "list"
       return list(array)
     elsif reply == "play"
-      play (array)
+      return play (array)
     #else
     #  reply = "exit"
     end
